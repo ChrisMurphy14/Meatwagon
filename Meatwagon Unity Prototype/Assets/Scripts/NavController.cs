d696ab9aa95ea807c2b27c223fc1df3afec32fdb
LEAKYFINGERS <weepingdigits@gmail.com>
2024-06-14 12:07:56 +1000

    More work on navigation tile system

--- Meatwagon Unity Prototype/Assets/Scripts/NavController.cs
+++ Meatwagon Unity Prototype/Assets/Scripts/NavController.cs
@@ -1,12 +1,14 @@
 //////////////////////////////////////////////////
 // Author:              Chris Murphy
 // Date created:        13.06.24
-// Date last edited:    13.06.24
+// Date last edited:    14.06.24
 // References:          https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
 //////////////////////////////////////////////////
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using UnityEngine;
 
 namespace Meatwagon
@@ -16,95 +18,223 @@ namespace Meatwagon
     {
         public List<NavTile> NavTiles;
 
-        public void CalculateShortestPathToTile(NavTile startTile, NavTile goalTile)
+        public bool DoesNavTileExistInList(NavTile tile)
         {
+            foreach (NavTile listTile in NavTiles)
+            {
+                if (listTile == tile)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
 
+        public List<NavTile> CalculateShortestPathToTile(NavTile startTile, NavTile goalTile)
+        {
+            if (startTile == goalTile)
+            {
+                Debug.LogWarning("The startTile argument cannot be the same tile as the goalTile.");
+                return null;
+            }
+            if (DoesNavTileExistInList(startTile) == false)
+            {
+                Debug.LogWarning("The startTile argument provided to the CalculateShortestPathToTile() function doesn't exist within the NavTiles list of the NavController.");
+                return null;
+            }
+            if (DoesNavTileExistInList(goalTile) == false)
+            {
+                Debug.LogWarning("The goalTile argument provided to the CalculateShortestPathToTile() function doesn't exist within the NavTiles list of the NavController.");
+                return null;
+            }
 
-            //bool startTileInList = false;
-            //bool goalTileInList = false;
-            //foreach (NavTile tile in NavTiles)
-            //{
-            //    if (!startTileInList && tile == startTile)
-            //    {
-            //        startTileInList = true;
-            //    }
-            //    if (!goalTileInList && tile == goalTileInList)
-            //    {
-            //        goalTileInList = true;
-            //    }
-            //}
-            //if (!startTileInList || !goalTileInList)
-            //{
-            //    Debug.LogWarning("The start and/or goal tiles for calculating the shortest path between don't exist in the NavController's NavTiles list.");
-            //    return;
-            //}
+            // Outcomes:
+            // - Find a valid path between the two tiles and return the shortest path
+            // - Find there is no valid path between the two tiles and return null/empty list?
 
-            int infinity = 9999;
+            int infiniteDistance = 9999; // Used as the default value for tiles that haven't been visited.
 
-            List<DijkstraNode> unvisitedSet = new List<DijkstraNode>();
+            // Steps 1 + 2
+            List<NavTile> unvisitedSet = new List<NavTile>();
+            int currentNodeIndex = -1;
             for (int i = 0; i < NavTiles.Count; ++i)
             {
-                DijkstraNode node;
-                node.Tile = NavTiles[i];
                 if (NavTiles[i] == startTile)
                 {
-                    node.ShortestDistFromStart = 0; // Set starting tile distance value to 0;
+                    NavTiles[i].ShortestDistance = 0; // Set starting tile distance value to 0;
+                    Debug.Log("Starting node index is: " + currentNodeIndex);
                 }
                 else
                 {
-                    node.ShortestDistFromStart = infinity; // Counts as the 'infinite distance' value in this case for Dijkstra's algorithm.
+                    NavTiles[i].ShortestDistance = infiniteDistance; // Counts as the 'infinite distance' value in this case for Dijkstra's algorithm.
                 }
 
-                unvisitedSet.Add(node);
-                Debug.Log("Added node to unvisited set with name " + node.Tile.name + " and starting distance value of " + node.ShortestDistFromStart);
+                unvisitedSet.Add(NavTiles[i]);
+                Debug.Log("Added node to unvisited set with name " + NavTiles[i].name + " and starting distance value of " + NavTiles[i].ShortestDistance);
             }
 
-            // Get the node from the unvisited set with the shortest distance from the starting node.
-            int currentNodeIndex = 0;
-            for (int i = 0; i < unvisitedSet.Count; ++i)
+
+            List<NavTile> shortestPath = new List<NavTile>();
+            // Step 3
+            while (shortestPath.Any() == false)
             {
-                if (unvisitedSet[i].ShortestDistFromStart != infinity && unvisitedSet[i].ShortestDistFromStart < unvisitedSet[currentNodeIndex].ShortestDistFromStart)
+                // If the unvisited set is empty and no path has been found, there's no valid path between the start and goal tiles.
+                if (unvisitedSet.Any() == false)
                 {
-                    currentNodeIndex = 1;
+                    return null;
                 }
+                // Else if all the remaining distances in the unvisited set are equal to infinity, no further tiles can be reached and no valid path exists.
+                bool allDistancesAreInfinite = true;
+                foreach (NavTile tile in unvisitedSet)
+                {
+                    if (tile.ShortestDistance != infiniteDistance)
+                    {
+                        allDistancesAreInfinite = false;
+                        break;
+                    }
+                }
+                if (allDistancesAreInfinite)
+                {
+                    return null;
+                }
+
+                // Get the node in the unvisited set with the shortest distance.
+                currentNodeIndex = 0;
+                for (int i = 0; i < unvisitedSet.Count; i++)
+                {
+                    if (unvisitedSet[i].ShortestDistance < unvisitedSet[currentNodeIndex].ShortestDistance)
+                    {
+                        currentNodeIndex = i;
+                    }
+                }
+                if (unvisitedSet[currentNodeIndex] == goalTile)
+                {
+                    shortestPath = GetShortestPathOnceCalculated(startTile, goalTile);
+                    return shortestPath;
+                }
+
+                // Step 4
+                foreach (NavTile adjacentTile in unvisitedSet[currentNodeIndex].ConnectedTiles)
+                {
+                    if (unvisitedSet[currentNodeIndex].ShortestDistance + 1 < adjacentTile.ShortestDistance)
+                    {
+                        adjacentTile.ShortestDistance = unvisitedSet[currentNodeIndex].ShortestDistance + 1;
+                    }
+                }
+
+                // Step 5
+                unvisitedSet.Remove(unvisitedSet[currentNodeIndex]);
             }
 
+            return null;
+        }
 
 
 
-                //List<NavTile> unvisitedSet = NavTiles;
+        private List<NavTile> GetShortestPathOnceCalculated(NavTile startTile, NavTile goalTile)
+        {
+            List<NavTile> shortestPath = new List<NavTile>();
+            NavTile shortestPathTile = goalTile;
 
-                //int[] tileDistancesFromStart = new int[NavTiles.Count];
-                //for (int i = 0; i < NavTiles.Count; i++)
-                //{
-                //    if (NavTiles[i] == startTile)
-                //    {
-                //        tileDistancesFromStart[i] = 0; // Set starting tile distance value to 0;
-                //    }
-                //    else
-                //    {
-                //        tileDistancesFromStart[i] = -1; // Counts as the 'infinite distance' value in this case for Dijkstra's algorithm.
-                //    }
-                //}
+            while (shortestPath.Any() == false || shortestPath[0] != startTile)
+            {
+                shortestPath.Insert(0, shortestPathTile);
 
-                //int shortestUnvisitedDistanceTileIndex = -1;
-                //for(int i = 0; i > unvisitedSet.Count; ++i)
-                //{
-                //    if()
-                //}
+                if (shortestPathTile == startTile)
+                {
+                    break;
+                }
 
+                NavTile nextShortestPathTile = shortestPathTile.ConnectedTiles[0];
+                foreach (NavTile adjacentTile in shortestPathTile.ConnectedTiles)
+                {
+                    if (adjacentTile.ShortestDistance < nextShortestPathTile.ShortestDistance)
+                    {
+                        nextShortestPathTile = adjacentTile;
+                    }
+                }
+
+                shortestPathTile = nextShortestPathTile;
             }
+
+            return shortestPath;
+        }
+        private NavTile SelectedTile;
+
+
+        private void Awake()
+        {
+            //foreach (NavTile tile in NavTiles)
+            //{
+            //    if (tile == null)
+            //    {
+            //        Debug.LogException(new NullReferenceException("An item in the NavTiles list equals null."));
+            //    }
+            //}
+        }
 
         private void Start()
         {
-            CalculateShortestPathToTile(NavTiles[0], NavTiles[3]); // DEBUG!
-        }
-    }
+            NavTiles = new List<NavTile>();
+            foreach (NavTile tile in GameObject.FindObjectsOfType<NavTile>())
+            {
+                NavTiles.Add(tile);
+            }
 
-    public struct DijkstraNode
-    {
-        public NavTile Tile;
-        public int ShortestDistFromStart;
+            //NavTile startTile = NavTiles[3];
+            //NavTile goalTile = NavTiles[0];
+
+            //List<NavTile> testPath = CalculateShortestPathToTile(startTile, goalTile); // DEBUG!
+            //Debug.Log("The shortest path between tiles " + startTile.name + " and " + goalTile.name + " is:");
+            //foreach (NavTile tile in testPath)
+            //{
+            //    Debug.Log(tile.name);
+            //}
+
+        }
+
+        private void Update()
+        {
+            foreach (NavTile tile in NavTiles)
+            {
+                if (tile.IsMouseOver())
+                {
+                    if(Input.GetMouseButtonDown(0))
+                    {
+                        if (SelectedTile != null)
+                        {
+                            SelectedTile.SetSelectedState(NavTile.SelectedState.Default);
+                        }
+
+                        
+                        tile.SetSelectedState(NavTile.SelectedState.Selected);
+                        SelectedTile = tile;
+                    }
+
+                    if (tile != SelectedTile)
+                    {
+                        
+
+                        List<NavTile> tilePath = CalculateShortestPathToTile(SelectedTile, tile);
+                        if(tilePath != null)
+                        {
+                            foreach(NavTile pathTile in tilePath)
+                            {
+                                if(pathTile != SelectedTile)
+                                {
+                                    pathTile.SetSelectedState(NavTile.SelectedState.Highlighted);
+                                }
+                            }
+                        }
+                    }
+                }
+                else if(tile != SelectedTile)
+                {
+                    tile.SetSelectedState(NavTile.SelectedState.Default);
+                }
+            }
+        }
     }
 }
--- Meatwagon Unity Prototype/Assets/Scripts/NavTile.cs
+++ Meatwagon Unity Prototype/Assets/Scripts/NavTile.cs
@@ -1,32 +1,148 @@
 //////////////////////////////////////////////////
 // Author:              Chris Murphy
 // Date created:        13.06.24
-// Date last edited:    13.06.24
+// Date last edited:    14.06.24
 //////////////////////////////////////////////////
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Meatwagon
 {
     // A tile within the scene which a character can inhabit/navigate to and from.
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(MeshRenderer))]
     [RequireComponent(typeof(Transform))]
     public class NavTile : MonoBehaviour
     {
-        public List<NavTile> ConnectedTiles;
+        public enum SelectedState
+        {
+            Default,
+            Highlighted,
+            Selected
+        }
 
+        public List<NavTile> ConnectedTiles;
+        public Material defaultMaterial;
+        public Material highlightMaterial;
+        public Material SelectedMaterial;
+        public int ShortestDistance;
+
+        public bool IsMouseOver()
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (_boxCollider.OverlapPoint(mousePosition))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public SelectedState GetSelectedState()
+        {
+            return _selectedState;
+        }
+
+        public void SetSelectedState(SelectedState selectedState)
+        {
+            switch(selectedState)
+            {
+                case SelectedState.Default:
+                    {
+                        _meshRenderer.material = defaultMaterial;
+                        _selectedState = SelectedState.Default;
+
+                        break;
+                    }
+                case SelectedState.Highlighted:
+                    {
+                        _meshRenderer.material = highlightMaterial;
+                        _selectedState = SelectedState.Highlighted;
+
+                        break;
+                    }
+                case SelectedState.Selected:
+                    {
+                       
+
+                        _meshRenderer.material = SelectedMaterial;
+                        _selectedState = SelectedState.Selected;
+
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+        }
+
+
+
+        private BoxCollider2D _boxCollider;
+        private MeshRenderer _meshRenderer;
+        private SelectedState _selectedState;
+
+        private void Awake()
+        {
+            _boxCollider = GetComponent<BoxCollider2D>();
+            _meshRenderer = GetComponent<MeshRenderer>();
+        }
+
+        private void Start()
+        {
+            foreach (NavTile tile in GameObject.FindObjectsOfType<NavTile>())
+            {
+                if ((tile.transform.position - this.transform.position).magnitude == 1.0f)
+                {
+                    ConnectedTiles.Add(tile);
+                }
+            }
+        }
+
+        private void Update()
+        {
+            //if (Input.GetMouseButtonDown(0))
+            //{
+            //Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //if (_boxCollider.OverlapPoint(mousePosition))
+            //{
+            //    if (Input.GetMouseButtonDown(0))
+            //    {
+            //        SetSelectedState(SelectedState.Selected);
+            //    }
+            //    else
+            //    {
+            //        SetSelectedState(SelectedState.Highlighted);
+            //    }
+            //}
+            //else if(GetSelectedState() == SelectedState.Highlighted)
+            //{
+            //    SetSelectedState(SelectedState.Default);
+            //}
+            //}
+        }
 
 
         private void OnDrawGizmos()
         {
-            Gizmos.color = UnityEngine.Color.yellow;
-            Gizmos.DrawWireCube(this.transform.position, Vector3.one);
+            //Gizmos.color = UnityEngine.Color.yellow;
+            //Gizmos.DrawWireCube(this.transform.position, Vector3.one);
 
-            Gizmos.color = UnityEngine.Color.blue;
-            foreach (NavTile connectedTile in ConnectedTiles)
+            if (ConnectedTiles.Any() != false)
             {
-                Gizmos.DrawLine(this.transform.position, connectedTile.GetComponent<Transform>().position);
+                Gizmos.color = UnityEngine.Color.blue;
+                foreach (NavTile connectedTile in ConnectedTiles)
+                {
+                    Gizmos.DrawLine(this.transform.position, connectedTile.GetComponent<Transform>().position);
+                }
             }
         }
     }
